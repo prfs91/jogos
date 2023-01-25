ca41481e83973d9e0880fc1c08abdad0e69c6f8a
= <=>
2023-01-25 07:40:43 -0400

    Nível de dificuldade.

--- adivinhacao.py
+++ adivinhacao.py
@@ -5,9 +5,19 @@ print("Bem-vindo no jogo de Adivinhação!")
 print("*********************************")
 
 numero_secreto = random.randrange(1, 101)
-total_de_tentativas = 3
+total_de_tentativas = 0
 
-print(numero_secreto)
+print("Qual o nível de dificuldade?")
+print("(1) Fácil (2) Médio (3) Difícil")
+
+nivel = int(input("Defina o nível: "))
+
+if (nivel == 1):
+    total_de_tentativas = 20
+elif (nivel == 2):
+    total_de_tentativas = 10
+else:
+    total_de_tentativas = 5
 
 for rodada in range(1, total_de_tentativas + 1):
     print("Tentativa {} de {}.".format(rodada, total_de_tentativas))
