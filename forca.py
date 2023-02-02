cf5fb5e5090543f38d78d2151d6ddb756819a58b
= <=>
2023-02-02 10:06:46 -0400

    Finding the letters. Implementation of kicks, errors and successes of the game.

--- forca.py
+++ forca.py
@@ -4,22 +4,39 @@ def jogar():
     print("** Bem-vindo no jogo de Forca! **")
     print("*********************************")
 
-    palavra_secreta = "banana"
+    palavra_secreta = "banana".upper()
+    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
+    letras_faltando = str(letras_acertadas.count("_"))
 
     enforcou = False
     acertou = False
+    erros = 0
+
+    print(letras_acertadas)
 
     while (not enforcou and not acertou):
 
         chute = input("Qual letra?")
+        chute = chute.strip().upper()
 
-        index = 0
-        for letra in palavra_secreta:
-            if (chute == letra):
-                print("Encontrei a letra {} na posição {}.".format(letra, index))
-            index = index + 1
-        print("jogando...")
+        if (chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if (chute == letra):
+                    letras_acertadas[index] = letra
+                index += 1
 
+        else:
+            erros += 1
+
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
+        print(letras_acertadas)
+
+    if (acertou):
+        print("Você ganhou!")
+    else:
+        print("Você perdeu!")
     print("Fim do jogo.")
 
 if (__name__ == "__main__"):
